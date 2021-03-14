ff9ebc904801bde956c14d33d5443897d73581d4
Ildar <yafarov13@gmail.com>
2021-03-14 15:01:13 +0300

    Homework8

---
+++ 3 шахматная доска (версия где описан каждый шаг)/scripts/positions.js
@@ -0,0 +1,113 @@
+let positions = [
+    //названия и координаты белых фигур, кроме пешек
+    {
+        coordCol: 'a',
+        coordRow: 1,
+        figure: 'rook',
+        color: 'white'
+    },
+    {
+        coordCol: 'b',
+        coordRow: 1,
+        figure: 'knight',
+        color: 'white'
+    },
+    {
+        coordCol: 'c',
+        coordRow: 1,
+        figure: 'bishop',
+        color: 'white'
+    },
+    {
+        coordCol: 'd',
+        coordRow: 1,
+        figure: 'queen',
+        color: 'white'
+    },
+    {
+        coordCol: 'e',
+        coordRow: 1,
+        figure: 'king',
+        color: 'white'
+    },
+    {
+        coordCol: 'f',
+        coordRow: 1,
+        figure: 'bishop',
+        color: 'white'
+    },
+    {
+        coordCol: 'g',
+        coordRow: 1,
+        figure: 'knight',
+        color: 'white'
+    },
+    {
+        coordCol: 'h',
+        coordRow: 1,
+        figure: 'rook',
+        color: 'white'
+    },
+
+
+
+
+    //названия и координаты черных фигур, кроме пешек
+    {
+        coordCol: 'a',
+        coordRow: 8,
+        figure: 'rook',
+        color: 'black'
+    },
+    {
+        coordCol: 'b',
+        coordRow: 8,
+        figure: 'knight',
+        color: 'black'
+    },
+    {
+        coordCol: 'c',
+        coordRow: 8,
+        figure: 'bishop',
+        color: 'black'
+    },
+    {
+        coordCol: 'd',
+        coordRow: 8,
+        figure: 'queen',
+        color: 'black'
+    },
+    {
+        coordCol: 'e',
+        coordRow: 8,
+        figure: 'king',
+        color: 'black'
+    },
+    {
+        coordCol: 'f',
+        coordRow: 8,
+        figure: 'bishop',
+        color: 'black'
+    },
+    {
+        coordCol: 'g',
+        coordRow: 8,
+        figure: 'knight',
+        color: 'black'
+    },
+    {
+        coordCol: 'h',
+        coordRow: 8,
+        figure: 'rook',
+        color: 'black'
+    },
+];
+
+let chessSymbols = {
+    king: '&#9812',
+    queen: '&#9813',
+    rook: '&#9814',
+    bishop: '&#9815',
+    knight: '&#9816',
+    pawn: '&#9817'
+}
